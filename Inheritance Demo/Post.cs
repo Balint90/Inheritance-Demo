8e39796ce6bb0d0d9ef18cd405eaf5fda7ed0f0f
Bálint Csertán <csertanb@hotmail.com>
2022-01-02 22:56:31 +0100

    Post Class' Props, Constructor, GetNextID, Update, and ToString override add

---
+++ Inheritance Demo/Post.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Demo
+{
+    class Post
+    {
+        private static int currentPostId;
+
+        //properties
+        protected int ID { get; set; }
+        protected string Title { get; set; }
+        protected string SendByUsername { get; set; }
+        protected bool IsPublic { get; set; }
+
+        //default constructor. 
+        public Post()
+        {
+            ID = 0;
+            Title = "My First Post";
+            IsPublic = true;
+            SendByUsername = "Balint Csertan";
+        }
+
+        //instance constructor with 3 parameters
+        public Post(string title, bool isPublic, string sendByUsername)
+        {
+            this.ID = GetNextID(); //need a method to create a new ID for each post
+            this.Title = title;
+            this.IsPublic = isPublic;
+            this.SendByUsername = sendByUsername;
+        }
+
+        protected int GetNextID()
+        {
+            return currentPostId++;
+        }
+
+        public void Update(string title, bool isPublic)
+        {
+            this.Title = title;
+            this.IsPublic = isPublic;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} - {1} - by {2}", this.ID, this.Title, this.SendByUsername);
+        }
+    }
+}
