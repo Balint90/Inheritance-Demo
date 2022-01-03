8f972ea0620637103c0e10dc665a9af5a11e7a2f
Bálint Csertán <csertanb@hotmail.com>
2022-01-03 11:56:59 +0100

    Add ImagePost

--- Inheritance Demo/Program.cs
+++ Inheritance Demo/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Inheritance_Demo;
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Post post1 = new Post("Thanks for the birthday wishes", true, "Balint Csertan");
+Console.WriteLine(post1.ToString());
+
+ImagePost imagePost1 = new ImagePost("Check out my new shoes!", "Balint Csertan", "http://images.com/shoes", true);
+Console.WriteLine(imagePost1.ToString());
+Console.ReadLine();
